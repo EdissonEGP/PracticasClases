f61df2ce9815d4aad098e8f5f0c30d3a9db1c5f5
EdissonEGP <109998231+EdissonEGP@users.noreply.github.com>
2022-08-12 18:37:49 -0500

    Revert "Change-Id: I0000000000000000000000000000000000000000"
    This reverts commit 9690eeac80ec7f9520dc3bd6883fee6aaedfe26d.

--- digitalTemplate-master/src/test/java/com/Tests/Test01.java
+++ digitalTemplate-master/src/test/java/com/Tests/Test01.java
@@ -23,7 +23,7 @@ class Test01 {
 	@BeforeEach
 	public void setUp() {
 		
-		System.setProperty("webdriver.chrome.driver",".\\src\\test\\resources\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",".\\src\\test\\resources\\chomedriver\\chromedriver.exe");
 		
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
